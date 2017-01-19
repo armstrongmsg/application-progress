b68ddbee38b7b6ef4ed886252fe2c418223dbc2f
armstrongmsg <armstrongmsg@gmail.com>
2017-01-18 22:02:08 -0300

    Removed the log transformation in the visualization and added more analysis cases

--- analysis.R
+++ analysis.R
@@ -22,29 +22,70 @@ data$timestamp2 <- timestamp2
 data <- filter(data, stage != 4)
 
 # All caps together
-ggplot() + geom_line(data = data, aes(log(timestamp2), completion, colour = data2$stage, group = 1)) + 
+ggplot() + geom_line(data = data, aes(timestamp2, completion, colour = data$stage, group = 1)) + 
   facet_grid(. ~ cap)
 
 # Scale-free
 ggplot() + 
-  geom_line(data = data, aes(log(timestamp2), completion, colour = data2$stage, group = 1)) + 
+  geom_line(data = data, aes(timestamp2, completion, colour = data$stage, group = 1)) + 
   facet_grid(. ~ cap, scales = "free")
 
+ggsave("allcases.png")
+
 # Individual caps
-ggplot(filter(data, cap == "25"), aes(log(timestamp2), completion, colour = stage)) + 
+ggplot(filter(data, cap == "25"), aes(timestamp2, completion, colour = stage)) + 
   geom_line(group = 1) +
   ggtitle("CAP = 25")
 
-ggplot(filter(data, cap == "50"), aes(log(timestamp2), completion, colour = stage)) + 
+ggsave("cap25.png")
+
+ggplot(filter(data, cap == "50"), aes(timestamp2, completion, colour = stage)) + 
   geom_line(group = 1) +
   ggtitle("CAP = 50")
 
-ggplot(filter(data, cap == "75"), aes(log(timestamp2), completion, colour = stage)) + 
+ggsave("cap50.png")
+
+ggplot(filter(data, cap == "75"), aes(timestamp2, completion, colour = stage)) + 
   geom_line(group = 1) +
   ggtitle("CAP = 75")
 
-ggplot(filter(data, cap == "100"), aes(log(timestamp2), completion, colour = stage)) + 
+ggsave("cap75.png")
+
+ggplot(filter(data, cap == "100"), aes(timestamp2, completion, colour = stage)) + 
   geom_line(group = 1) +
   ggtitle("CAP = 100")
 
-ggsave("progress.png")
+ggsave("cap100.png")
+
+# Get the time spent in the stages
+stage.times <- data.frame(stage = c(), cap = c(), time = c())
+
+for (stage_ in c(0,1,2,3)) { 
+  for (cap_ in c(25,50,75,100)) { 
+    stage_data <- filter(data, stage == stage_ & cap == cap_) 
+    stage.times <- rbind(stage.times, data.frame(stage=c(stage_), cap=c(cap_), time=c(max(stage_data$timestamp2)-min(stage_data$timestamp2))))
+  }
+}
+
+# Get the ratio between the stage times using cap == 25% and cap == 100%
+stage.times.ratio.100.25 <- stage.times %>% group_by(stage) %>% summarise(ratio = time[1]/time[4])
+# Get the ratio between the stage times using cap == 50% and cap == 100%
+stage.times.ratio.100.50 <- stage.times %>% group_by(stage) %>% summarise(ratio = time[2]/time[4])
+# Get the ratio between the stage times using cap == 75% and cap == 100%
+stage.times.ratio.100.75 <- stage.times %>% group_by(stage) %>% summarise(ratio = time[3]/time[4])
+
+ggplot(stage.times.ratio.100.25, aes(stage, ratio)) + geom_bar(stat = "identity")
+ggplot(stage.times.ratio.100.50, aes(stage, ratio)) + geom_bar(stat = "identity")
+ggplot(stage.times.ratio.100.75, aes(stage, ratio)) + geom_bar(stat = "identity")
+
+stage.times.ratio.all <- data.frame(case=c(), stage=c(), ratio=c())
+stage.times.ratio.all <- rbind(stage.times.ratio.all, data.frame(case="25", stage=stage.times.ratio.100.25$stage, ratio=stage.times.ratio.100.25$ratio))
+stage.times.ratio.all <- rbind(stage.times.ratio.all, data.frame(case="50", stage=stage.times.ratio.100.50$stage, ratio=stage.times.ratio.100.50$ratio))
+stage.times.ratio.all <- rbind(stage.times.ratio.all, data.frame(case="75", stage=stage.times.ratio.100.75$stage, ratio=stage.times.ratio.100.75$ratio))
+
+ggplot(stage.times.ratio.all, aes(stage, ratio)) + 
+  geom_bar(stat = "identity") + 
+  facet_grid(. ~ case) +
+  ylab("ratio (cap X / cap 100%)")
+
+ggsave("ratio.png")
